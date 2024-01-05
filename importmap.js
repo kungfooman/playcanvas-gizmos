d484346a113b818c1cd57c8ab73f957e8f76c08c
kungfooman <lama12345@gmail.com>
2024-01-05 19:19:37 +0100

    importmap.js: use PlayCanvas from `npm install playcanvas`

--- importmap.js
+++ importmap.js
@@ -6,7 +6,17 @@ function importFile(content) {
   return "data:text/javascript;base64," + btoa(content);
 }
 const imports = {
-  "playcanvas": "/playcanvas-engine/src/index.js",
+  // BC = backwards compatibility of shaders
+  //"playcanvas": "/playcanvas-engine/src/index.js",
+  // Backwards compatibility of shaders
+  //"playcanvas": "/playcanvas-engine-jsdoc/src/index.js", // BC
+  "playcanvas": "./node_modules/playcanvas/build/playcanvas.dbg.mjs/index.js",
+  //"playcanvas": "/playcanvas-engine-jsdoc/build/playcanvas.dbg.mjs/index.js", // BC
+
+  // Lacking backwards compatibility of shaders, use/enable when everything is fixed!
+  //"playcanvas": "/playcanvas-engine-jsdoc/build/playcanvas.mjs/index.js",
+  //"playcanvas": "/playcanvas-engine-jsdoc/build/playcanvas.prf.mjs/index.js",
+  //"playcanvas": "/playcanvas-engine-jsdoc/build/playcanvas.min.mjs/index.js",
 };
 const importmap = document.createElement("script");
 importmap.type = "importmap";
